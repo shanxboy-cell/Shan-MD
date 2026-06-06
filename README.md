10565fc2d20e4f7bf2eae955f37df0a218b3e654
Shan-Boy...👻💗 <shanxboy2008@gmail.com>
2026-06-06 17:32:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Shan-MD
+## Shan-MD 👨‍💻 - A Javascript WhatsApp User Bot"
+Shan-MD has been designed to make things easier and fun while using WhatsApp. 👻❤️
+### `3.0.0 Version`
+😎 `The main goal of creating this bot is to fully leverage WhatsApp and simplify its functionality.`
+
+<br>
+<p align="center">  
+  <a href="https://files.catbox.moe/cpbsxt.jpg">
+    <img alt="shan-md" height="300" src="https://files.catbox.moe/cpbsxt.jpg">
+    
+  
+  </a>
+</p>  
+
+
+<br>
+
+<p align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=700&size=33&pause=1000&color=5513F7&width=435&lines=SHAN+MD+WHATSAPP+BOT+v3" alt="Typing SVG" /></a>
+</p>
+
+## 🇱🇰 ***New bot, latest version 🎉 [new reposity](https://github.com/SHAN-MD819/Shan-MD)***
---
+++ config.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+//gg
+module.exports = {
+SESSION_ID:'CAVCyRaZ#m2Q-2F6EK-657ax02jinK0b8HVRnzdULM4C9C0GiP3M', // methanata ube id eka daganim
+
+ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
+MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
+ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK, 
+SEEDR_MAIL: '',
+SEEDR_PASSWORD: '',
+SUDO: '',//
+DB_NAME: 'Shan',
+LANG: 'SI',
+OWNER_NUMBER: '94711726564',
+LOGO: process.env.LOGO === undefined ? 'https://telegra.ph/file/38a4ba6f7a51c15f5feaf.jpg' : process.env.LOGO
+
+};
+//GITHUB_AUTH_TdOKEN: 'ouvnI0xSDsmfWA1filVxx.SZ0vJGYkjlC5VX54U0e10',
