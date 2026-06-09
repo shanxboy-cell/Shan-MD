48c724b8efc48dc7945d06ba9ebdc4b5d219455f
Shan-Boy...👻💗 <shanxboy2008@gmail.com>
2026-06-09 16:51:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'', // methanata ube id eka daganim
+SESSION_ID:'rQN1UYBI#2_tOfnSeDGrBRLL1KDUS7dnalnjdCVk_Z5NynKjmaMM', // methanata ube id eka daganna
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
